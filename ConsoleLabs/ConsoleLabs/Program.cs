76378f4198fa82e2dcfa17630c5c202f31d58040
Unknown <egor9996@gmail.com>
2019-03-14 11:06:15 +0300

    add normal files

---
+++ ConsoleLabs/ConsoleLabs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labs;  
+
+namespace ConsoleLabs
+{
+    class Program
+    {
+        static void Lab_12_13()
+        {
+            Console.WriteLine("Лабораторная работа №13");
+            Console.WriteLine("---");
+            var tree = new BinaryTree(); 
+
+            tree.Insert(8); 
+            tree.Insert(3); 
+            tree.Insert(10);  
+            tree.Insert(14); 
+            tree.Insert(13);  
+            tree.Insert(1); 
+            tree.Insert(6); 
+            tree.Insert(4); 
+            tree.Insert(7); 
+
+            BinaryTreeExtensions.Print(tree);
+
+            Console.WriteLine("Лабораторная работа №12");
+            Console.WriteLine("---");
+
+            BinaryTreeExtensions.CLR(tree); 
+            Console.WriteLine();
+            BinaryTreeExtensions.LCR(tree); 
+            Console.WriteLine();
+            BinaryTreeExtensions.LRC(tree); 
+
+            Console.ReadKey(); 
+        }
+         
+         
+        static void Main(string[] args)
+        {
+            Lab_12_13();  
+        }  
+    }
+}
